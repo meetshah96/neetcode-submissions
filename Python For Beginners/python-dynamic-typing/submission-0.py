dcca5326752287e5f3732068f9d199dd34a58134
meetshah96 <meetshah7166@gmail.com>
2026-05-31 11:54:25 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "test"
+print(type(variable))
+variable = ["test"]
+print(type(variable))
