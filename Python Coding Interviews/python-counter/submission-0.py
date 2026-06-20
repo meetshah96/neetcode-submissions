41998344bc4e0b455e02a52cf0fe7ab950e6bb05
meetshah96 <meetshah7166@gmail.com>
2026-06-20 12:27:09 +0530

    Add: python-counter - submission-0

---
+++ Python Coding Interviews/python-counter/submission-0.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from typing import Counter as CounterType
+
+
+def count_chars(s1: str, s2: str) -> CounterType:
+    s = s1+s2
+    counter = Counter(s)
+    return counter
+  
+
+# do not modify below this line
+print(count_chars("hello", "world"))
+print(count_chars("hello", "worldhello"))
+print(count_chars("areallylongstring", "heyhowisitgoing"))
